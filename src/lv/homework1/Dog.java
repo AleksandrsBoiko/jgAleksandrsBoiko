e5c9a9f184c1a3cc10b3a3cd0cb95e1eb784f641
AleksandrsBoiko <aleksandrs.boiko@gmail.com>
2019-02-14 21:35:14 +0200

    Homework1

--- src/lv/homework1/Dog.java
+++ src/lv/homework1/Dog.java
@@ -5,10 +5,38 @@ package lv.homework1;
         private String color;
         private int age;
 
+        public void voice(){
+            System.out.println("vau vau");
+        }
+        public void eat(){
+            System.out.println("eat");
+        }
+        public void sleep(){
+            System.out.println("zzzzzz...");
+        }
+
        public String getName(){
-           return name;
+           return this.name;
        }
-       public void setName
+       public void setName(String name){
+           this.name = name;
+       }
+       public int getAge() {
+           return this.age;
+       }
+
+       public void setAge(int age) {
+           this.age = age;
+       }
+
+       public String getColor(){
+           return this.color;
+       }
+
+       public void setColor(String color){
+            this.color = color;
+
+       }
 
 
 }
--- src/lv/homework1/DogTest.java
+++ src/lv/homework1/DogTest.java
@@ -1,7 +1,20 @@
 package lv.homework1;
 
+import java.sql.SQLOutput;
+
 public class DogTest {
     public static void main(String[] args) {
+        Dog fluffy = new Dog();
+        fluffy.setName("fluffy");
+        fluffy.setColor("brown");
+        fluffy.setAge(5);
+        fluffy.eat();
+        fluffy.sleep();
+        fluffy.voice();
+
+        System.out.println("My dog's name is " + fluffy.getName() );
+        System.out.println(fluffy.getName() + "'s color is " + fluffy.getColor());
+        System.out.println("And she is " + fluffy.getAge() + " years old.");
 
 
     }
