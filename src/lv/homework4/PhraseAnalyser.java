a722844d74c707f7153fdb6e179d1e6a28b3b11b
AleksandrsBoiko <aleksandrs.boiko@gmail.com>
2019-03-07 14:51:58 +0200

    Homework4

---
+++ src/lv/homework4/PhraseAnalyser.java
@@ -0,0 +1,48 @@
+package lv.homework4;
+
+public class PhraseAnalyser {
+
+    public String analyse(String phrase) {
+
+
+        if ( phrase.charAt(0) == 'M'
+                && phrase.charAt(1) == 'a'
+                && phrase.charAt(2) == 'k'
+                && phrase.charAt(3) == 'e'
+                && phrase.charAt(phrase.length() - 11) == 'g'
+                && phrase.charAt(phrase.length() - 10) == 'r'
+                && phrase.charAt(phrase.length() - 9) == 'e'
+                && phrase.charAt(phrase.length() - 8) == 'a'
+                && phrase.charAt(phrase.length() - 7) == 't'
+                && phrase.charAt(phrase.length() - 6) == ' '
+                && phrase.charAt(phrase.length() - 5) == 'a'
+                && phrase.charAt(phrase.length() - 4) == 'g'
+                && phrase.charAt(phrase.length() - 3) == 'a'
+                && phrase.charAt(phrase.length() - 2) == 'i'
+                && phrase.charAt(phrase.length() - 1) == 'n') {
+
+            return "It stands no change..";
+        } else if ( (phrase.charAt(0) == 'M'
+                && phrase.charAt(1) == 'a'
+                && phrase.charAt(2) == 'k'
+                && phrase.charAt(3) == 'e')
+                || (phrase.charAt(phrase.length() - 11) == 'g'
+                && phrase.charAt(phrase.length() - 10) == 'r'
+                && phrase.charAt(phrase.length() - 9) == 'e'
+                && phrase.charAt(phrase.length() - 8) == 'a'
+                && phrase.charAt(phrase.length() - 7) == 't'
+                && phrase.charAt(phrase.length() - 6) == ' '
+                && phrase.charAt(phrase.length() - 5) == 'a'
+                && phrase.charAt(phrase.length() - 4) == 'g'
+                && phrase.charAt(phrase.length() - 3) == 'a'
+                && phrase.charAt(phrase.length() - 2) == 'i'
+                && phrase.charAt(phrase.length() - 1) == 'n')) {
+            return "It could be worse";
+        }
+
+        return "It is fine, really";
+
+
+    }
+
+}
---
+++ src/lv/homework4/PhraseAnalyserTest.java
@@ -0,0 +1,15 @@
+package lv.homework4;
+
+public class PhraseAnalyserTest {
+
+    public static void main(String[] args) {
+
+        PhraseAnalyser analyser = new PhraseAnalyser();
+
+        String result = analyser.analyse("Make education bad again");
+
+        System.out.println(result);
+
+
+    }
+}
