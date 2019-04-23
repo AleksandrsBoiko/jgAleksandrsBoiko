ff6eefd3ce2f1344bf78306c327bc571821dec87
Aleksandrs Boiko <aleksandrs.boiko@gmail.com>
2019-04-23 21:58:29 +0300

    homework6

--- src/lv/homework6/NumberService.java
+++ src/lv/homework6/NumberService.java
@@ -3,7 +3,8 @@ package lv.homework6;
 public class NumberService {
 
     public int sum;
-    public int i;
+    public int evenNumberCount;
+
 
     public int sumInRange(int a, int b) {
         if (a < b) {
@@ -20,24 +21,30 @@ public class NumberService {
         return sum;
     }
 
+
     public int getEvenNumberCount(int a, int b) {
+        evenNumberCount = 0;
         if (a < b) {
             for (int i = a; i <= b; i++) {
                 if (i % 2 != 0) {
                     continue;
                 }
+                evenNumberCount = evenNumberCount+1;
             }
+
         }
          else{
-
+                evenNumberCount = 0;
                 for (int i = a; i >= b; i--) {
                     if (i % 2 != 0) {
                         continue;
                     }
+                    evenNumberCount = evenNumberCount+1;
                 }
             }
 
 
-        return i;
+        return evenNumberCount;
+
     }
 }
--- src/lv/homework6/NumberServiceTest.java
+++ src/lv/homework6/NumberServiceTest.java
@@ -8,20 +8,30 @@ public class NumberServiceTest {
 
         numberService.sumInRange(5,10);
 
-        System.out.println("Sum In Range");
+        System.out.println("\n" + "Sum In Range");
         System.out.println(" a = 5, b = 10 ");
         System.out.println("Sum = " + numberService.sum);
 
         numberService.sumInRange(15,8);
 
-        System.out.println("Sum In Range");
+        System.out.println("\n" + "Sum In Range");
         System.out.println(" a = 15, b = 8 ");
         System.out.println("Sum = " + numberService.sum);
 
         numberService.getEvenNumberCount(5,10);
 
-        System.out.println(numberService.i);
+        System.out.println("\n" + "Even number count");
+        System.out.println(" a = 5, b = 10 ");
+        System.out.println("Even number cout = " + numberService.evenNumberCount);
 
+        System.out.println(numberService.evenNumberCount);
 
+        numberService.getEvenNumberCount(15,7);
+
+        System.out.println("\n" + "Even number count");
+        System.out.println(" a = 15, b = 7 ");
+        System.out.println("Even number cout = " + numberService.evenNumberCount);
+
+        System.out.println(numberService.evenNumberCount);
     }
 }
