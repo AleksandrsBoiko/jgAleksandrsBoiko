6c48d66cdd2ac1170427a6b176b1e4062e4c55b6
Aleksandrs Boiko <aleksandrs.boiko@gmail.com>
2019-04-25 01:15:33 +0300

    homework

--- src/lv/homework11/ShoppingList.java
+++ src/lv/homework11/ShoppingList.java
@@ -5,14 +5,24 @@ import java.util.List;
 
 public class ShoppingList {
 
-    private static List<Product> createProduct(){
+    List<Product> shoppingList = new ArrayList<>();
+
+    public List<Product> addProduct(String productName, double price, String category){
+
+        shoppingList.add(new Product(productName, price, category));
+
+        return shoppingList;
+    }
+
+
+   /* public static List<Product> createProduct(){
 
         List<Product> shoppingList = new ArrayList<>();
 
         shoppingList.add(new Product("Milk", 1.20, "food"));
 
         return shoppingList;
-    }
+    }*/
 
 
 }
--- src/lv/homework11/ShoppingListTest.java
+++ src/lv/homework11/ShoppingListTest.java
@@ -1,4 +1,13 @@
 package lv.homework11;
 
 public class ShoppingListTest {
+
+    public static void main(String[] args) {
+
+        ShoppingList shoppingList = new ShoppingList();
+        shoppingList.addProduct("Milk", 1.20, "food");
+
+
+
+    }
 }
