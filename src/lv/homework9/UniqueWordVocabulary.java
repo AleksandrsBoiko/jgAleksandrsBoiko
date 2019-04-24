ccedacf12f92d9a13dac9e8d556531ff428cfa33
Aleksandrs Boiko <aleksandrs.boiko@gmail.com>
2019-04-24 05:20:07 +0300

    homework9

--- src/lv/homework9/UniqueWordVocabulary.java
+++ src/lv/homework9/UniqueWordVocabulary.java
@@ -2,22 +2,26 @@ package lv.homework9;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Scanner;
 
 public class UniqueWordVocabulary {
 
     Set<String>vocabulary = new HashSet<>();
 
-    public static void addWord(Set<String>vocabulary) {
-
-        addWord(vocabulary);
-
-    }
-
-    private static void printToConsole(Set<String>vocabulary) {
+        private static void printToConsole(Set<String>vocabulary) {
         System.out.println(vocabulary);
     }
 
+    private static void addWord(Set<String> vocabulary) {
+        Scanner wordScanner = new Scanner(System.in);
+        String words = wordScanner.nextLine();
+        if (words.equals("")) {
+            System.out.println("can't be empty");
+        } else {
+            vocabulary.add(words);
+        }
 
+    }
 
 
 
