bc4f2af36b379c97bbbd8a0c0953510ccf833afe
Aleksandrs Boiko <aleksandrs.boiko@gmail.com>
2019-04-24 19:21:24 +0300

    homework9

--- src/lv/homework9/UniqueWordVocabulary.java
+++ src/lv/homework9/UniqueWordVocabulary.java
@@ -2,9 +2,39 @@ package lv.homework9;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Scanner;
 
 public class UniqueWordVocabulary {
+
+    private Set<String> vocabulary = new HashSet<>();
+
+    public void addWord(String word) {
+
+        if (word.isEmpty()) {
+            System.out.println("Can't be empty");
+        } else {
+            vocabulary.add(word);
+        }
+    }
+        public void printToConsole () {
+            for (String listOfWords : vocabulary) {
+                System.out.println(listOfWords);
+            }
+        }
+        public void getUniqueWords () {
+            System.out.println("Vocabulary word count: " + vocabulary.size());
+        }
+
+
+}
+
+
+
+
+
+
+
+
+/* public class UniqueWordVocabulary {
     public static void main(String[] args) {
 
 
@@ -16,7 +46,7 @@ public class UniqueWordVocabulary {
 
         System.out.println(vocabulary);
 
-       /* public static void addWord (Set < String > vocabulary) {
+        public static void addWord (Set < String > vocabulary) {
             Scanner wordScanner = new Scanner(System.in);
             String words = wordScanner.nextLine();
             if (words.equals("")) {
@@ -32,6 +62,4 @@ public class UniqueWordVocabulary {
         }*/
 
 
-    }
 
-}
--- src/lv/homework9/UniqueWordVocabularyTest.java
+++ src/lv/homework9/UniqueWordVocabularyTest.java
@@ -1,10 +1,17 @@
-/*
+
 package lv.homework9;
 
 public class UniqueWordVocabularyTest {
     public static void main(String[] args) {
         UniqueWordVocabulary uniqueWordVocabulary = new UniqueWordVocabulary();
         uniqueWordVocabulary.addWord("tree");
+        uniqueWordVocabulary.addWord("car");
+        uniqueWordVocabulary.addWord("house");
+        uniqueWordVocabulary.addWord("");
+        uniqueWordVocabulary.printToConsole();
+        uniqueWordVocabulary.getUniqueWords();
+
+
     }
 }
-*/
+
