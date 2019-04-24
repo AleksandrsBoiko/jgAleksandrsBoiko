940bedb9b4938167d0074c7d645a4caff81ea7f3
Aleksandrs Boiko <aleksandrs.boiko@gmail.com>
2019-04-24 18:53:52 +0300

    homework9

--- src/lv/homework9/UniqueWordVocabulary.java
+++ src/lv/homework9/UniqueWordVocabulary.java
@@ -5,24 +5,33 @@ import java.util.Set;
 import java.util.Scanner;
 
 public class UniqueWordVocabulary {
+    public static void main(String[] args) {
 
-    Set<String>vocabulary = new HashSet<>();
 
-        private static void printToConsole(Set<String>vocabulary) {
+        Set<String> vocabulary = new HashSet<>();
+
+        vocabulary.add("tree");
+        vocabulary.add("house");
+        vocabulary.add("{car");
+
         System.out.println(vocabulary);
-    }
 
-    private static void addWord(Set<String> vocabulary) {
-        Scanner wordScanner = new Scanner(System.in);
-        String words = wordScanner.nextLine();
-        if (words.equals("")) {
-            System.out.println("can't be empty");
-        } else {
-            vocabulary.add(words);
+       /* public static void addWord (Set < String > vocabulary) {
+            Scanner wordScanner = new Scanner(System.in);
+            String words = wordScanner.nextLine();
+            if (words.equals("")) {
+                System.out.println("can't be empty");
+            } else {
+                vocabulary.add(words);
+            }
+
         }
 
+        public static void printToConsole (Set < String > vocabulary) {
+            System.out.println(vocabulary);
+        }*/
+
+
     }
-
-
 
 }
--- src/lv/homework9/UniqueWordVocabularyTest.java
+++ src/lv/homework9/UniqueWordVocabularyTest.java
@@ -1,4 +1,10 @@
+/*
 package lv.homework9;
 
 public class UniqueWordVocabularyTest {
+    public static void main(String[] args) {
+        UniqueWordVocabulary uniqueWordVocabulary = new UniqueWordVocabulary();
+        uniqueWordVocabulary.addWord("tree");
+    }
 }
+*/
